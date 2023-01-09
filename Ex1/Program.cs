be135f09ef264638fbd03af0964035ff3006dba2
Nessa <la-nessa@mail.ru>
2023-01-09 17:25:16 +0300

    Ex1

---
+++ Ex1/Program.cs
@@ -0,0 +1,43 @@
+﻿void PrintArray(int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+     { 
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] +  " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SortDataRows(int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    { 
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            int maxPosition = j;
+            for (int k = j +1; k < array.GetLength(1); k++)
+            {
+                if (array[i, k] > array[i, maxPosition])
+                {
+                    int temporary = array[i, k];
+                    array[i, k] = array[i, maxPosition];
+                    array[i, maxPosition] = temporary;
+                }
+            }
+        }
+    }
+}
+
+int[,] arr = new int [3,4] 
+{
+    {4, 2, 1, 3 },
+    {8, 3, 10, 1 },
+    {5, 7, 11, 5 }
+};
+
+PrintArray(arr);
+SortDataRows(arr);
+Console.WriteLine("Массив после расстановки элементов в порядке убывания в каждой строке:");
+PrintArray(arr);
